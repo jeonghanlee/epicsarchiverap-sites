bb9c64c3617ec5a2a2cb51f5455734f794460269
Jeong Han Lee <jeonghan.lee@gmail.com>
2016-11-23 14:55:51 +0100

    add the archiver feature

--- aa_scripts/getData.py
+++ aa_scripts/getData.py
@@ -34,7 +34,11 @@
 #           - introduce src and target location of the extracted file,
 #  - 0.4.0  Thursday, November 17 12:30:40 CET 2016
 #           - clean up and fix the pattern
-# 
+#  - 0.4.1  Wednesday, November 23 13:50:36 CET 2016,
+#           - fix the hard-coded local time according to UTC
+#           - add "archived" the entire target directory in target path
+#           - remove the intermediate path
+ 
 #    An example in cronjob (crontab -e) in every 5 mins
 #
 #  
@@ -44,7 +48,15 @@
 #    PV list : test_ioc_pv_list
 #    Mean    : no
 
-# */5 *  * * * export DISPLAY=:0.0 && /usr/bin/python /home/aauser/epicsarchiverap-sites/aa_scripts/getData.py -i 10.4.3.86 -d 1 -t /var/www/data/ -f ics_pv_list  >/dev/null 2>&1
+# */5 *  * * * export DISPLAY=:0.0 && /usr/bin/python /home/aauser/epicsarchiverap-sites/aa_scripts/getData.py -i 10.4.3.86 -d 1 -t /var/www/data/ -f ics_pv_list -z  >/dev/null 2>&1
+
+# Get date from Archiver Appliance to put them in ${HOME}/Archappl_ipaddress/CURRENT_TIME
+# Compress that directory and put them in ${HOME}/Archappl_ipaddress/
+# /usr/bin/python /home/aauser/epicsarchiverap-sites/aa_scripts/getData.py -i 10.0.4.22 -d 1  -f test_ioc_pv_list -t ${HOME} -z
+# /usr/bin/python /home/aauser/epicsarchiverap-sites/aa_scripts/getData.py -i 10.0.4.22 -d 1  -t ${HOME} -z -f test_ioc_pv_list
+
+
+
 
 
 import os
@@ -58,6 +70,7 @@ import urllib2
 import json
 
 from datetime import timedelta, datetime, time, date, tzinfo
+from shutil import make_archive
 
 # Theses ports should be the same as appliances.xml
 # e.g. /opt/archappl/appliances.xml
@@ -100,30 +113,30 @@ def setRawRetUrl(url):
 
 def getSelectedPVs(url, args):
 
+    script_path    = os.path.dirname(os.path.realpath(__file__))
     pv_list = []
     #
     #   Want to use "list" because pv_list, which is "return values from AA" is list
     #    
     if args.file :
-        script_path    = os.path.dirname(os.path.realpath(__file__))
         input_filename = script_path + "/" + args.file
         lines          = [line.strip() for line in open(input_filename)]
     else:
         # no input filter file exists, set all PVs
         lines = ['*']
 
-    if args.verbose:
-        print "getSelectedPVs function "
-        print "url, args      :", url, args
-        print "script_path    :", script_path
-        print "input_filename :", input_filename
-        print "type, lines    :", type(lines), lines
-        print "pattern        :", args.pattern
-        
 
     url_src = url + "getAllPVs" + args.pattern
 
-    if args.verbose : print "GetAllPV url : ", url_src
+    if args.verbose : 
+        print "getSelectedPVs function "
+        print "url, args      :", url, args
+        print "script_path    :", script_path
+        if args.file : 
+            print "input_filename :", input_filename
+            print "type, lines    :", type(lines), lines
+        if args.pattern: 
+            print "pattern        :", args.pattern
 
     resp    = urllib2.urlopen(url_src)
     patternMatchingPVs = json.load(resp)
@@ -152,12 +165,14 @@ def main():
     parser.add_argument("-p", "--pattern", help="?pv=xxx&limit=nnn",  default ="")
     # "?pv=*calc*"
     # -p "?limit=100"
-    parser.add_argument("-v", "--verbose", help="output verbosity",              action="store_true")
-    parser.add_argument("-d", "--days",    help="days to monitor from now", type=float, default=1.0)
-    parser.add_argument("-f", "--file",    help="filename which has selected PV list",  default="")
-    parser.add_argument("-s", "--src",     help="source location of generated data file", default="/tmp/")
-    parser.add_argument("-t", "--target",  help="target location of data file", default= os.environ['PWD'] )
-    parser.add_argument("-m", "--mean",    help="data average during secs", default="")
+    parser.add_argument("-v", "--verbose",  help="output verbosity",              action="store_true")
+    parser.add_argument("-d", "--days",     help="days to monitor from now", type=float, default=1.0)
+    parser.add_argument("-f", "--file",     help="filename which has selected PV list",  default="")
+    #    parser.add_argument("-im", "--intermediate",  help="put the files in the intermediate dir first", default="")
+    # parser.add_argument("-t", "--target",   help="target location of data file", default=os.environ['HOME'] )
+    parser.add_argument("-m", "--mean",     help="data average during secs", default="")
+    parser.add_argument("-z", "--zip",      help="output files are zippped", action="store_true")
+    parser.add_argument("-t", "--targetpath", help="put the files in a directory directly", default=os.environ['HOME'] )
     
     args = parser.parse_args()
 
@@ -170,8 +185,8 @@ def main():
         print ">>> Default URL and Pattern are used as follows:"
         print ">>>  URL :" + url
         print ">>>  Pattern : " + args.pattern
-        print ">>>  Source  : " + args.src
-        print ">>>  Target  : " + args.target
+#        print ">>>  Source  : " + args.intermediate
+        print ">>>  Target  : " + args.targetpath
         print ">>>"
 
 
@@ -228,12 +243,7 @@ def main():
             print "hostname : ", hostname
             print "hostip   : ", hostip
             
-        report_filename = ""
-        dest_directory = args.target + "/"
-    
-        if not os.path.exists(dest_directory):
-            os.makedirs(dest_directory)
-            
+           
         mean_sstring = ""
         mean_estring = ""
         
@@ -241,13 +251,33 @@ def main():
             mean_sstring += 'mean_' + args.mean + '('
             mean_estring += ')'
             
+
+#        if args.intermediate:
+#            intermediate_directory = args.intermediate + "/";
+        if args.targetpath.startswith('/') : 
+            target_path = args.targetpath + "/";
+        else:
+            print "\nAbsolute TARGET PATH is needed";
+            sys.exit()
+
+        target_path += "Archappl_" + args.ip + "/";
+        
+        target_path_per_each_call = target_path + _now_iso_string + "/";
+    
+        if not os.path.exists(target_path_per_each_call):
+            try:
+                os.makedirs(target_path_per_each_call)
+
+            except OSError as exc: 
+                if exc.errno != errno.EEXIST:
+                    raise
+                    
         for pv in sorted(matchingPVs):
-            
+           
             if args.verbose:
                 print pv
-                
-                
-            report_filename = args.src + pv.replace(":", "_").lower() + ".txt"
+    
+            report_filename = pv.replace(":", "_").lower() + ".txt"
             queryString = '?pv='
             queryString += mean_sstring 
             queryString += pv
@@ -257,27 +287,30 @@ def main():
             queryString += '&'
             queryString += toString 
 
-            if args.verbose:
-                print "queryString : ",  queryString
-                print "url : ", url
 
             src_url=setJsonRetUrl(url) + queryString
 
+
             if args.verbose:
-                print src_url
+                print "queryString : ",  queryString
+                print "url         : ", url
+                print "source url  : ", src_url
+
 
             dataresp = urllib2.urlopen(src_url)
             data     = json.load(dataresp)
             
-
-   
-                 
             if data :
-
+    
                 if args.verbose:  print "Total Data Size " , len(data[0]['data'])
 
                 try :
-                    file = open(report_filename, "w")
+#                    if args.intermediate:
+#                        
+ #                       file = open(intermediate_directory + report_filename, "w");
+ #                   else :
+                    file = open(target_path_per_each_call  + report_filename, "w")
+
                     file.write("# \n")
                     file.write("# Filename    : " + report_filename  + "\n")
                     file.write("# PV name     : " + pv               + "\n")
@@ -300,20 +333,45 @@ def main():
                         
                     file.write(s)
                     file.close()
+ 
+                    # if args.intermediate:
+                    #     try :
+                        #     shutil.copy (intermediate_directory + report_filename, target_path_per_each_call)
+        
+                        # except shutil.Error as e:
+                        #     print('Error: %s' % e)
+                        #     # eg. source or destination doesn't exist
+                        # except IOError as e:
+                        #     print('Error: %s' % e.strerror)
 
-                    try :
-                        shutil.copy (report_filename, dest_directory)
-                    except shutil.Error as e:
-                        print('Error: %s' % e)
-                        # eg. source or destination doesn't exist
-                    except IOError as e:
-                        print('Error: %s' % e.strerror)
-
+                
                 except IOError, (errno, strerror):
                     print "I/O error(%s): %s" % (errno, strerror)
 
+
+
             else:
                 if args.verbose:  print "data no", pv
+
+        if args.zip:
+               
+            base_name = _now_iso_string
+            base_dir  = target_path_per_each_call
+            root_dir  = target_path
+            if args.verbose:
+                print "Base name : ", base_name
+                print "Base dir  : ", base_dir
+                print "root dir  : ", root_dir
+
+            try : 
+                os.chdir(root_dir)
+                shutil.make_archive(base_name, 'zip', target_path_per_each_call)
+
+            except shutil.Error as e:
+                print('Error: %s' % e)
+            except IOError as e:
+                print('Error: %s' % e.strerror)
+
 
     sys.exit()
 
